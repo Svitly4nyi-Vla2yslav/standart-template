04aebeac336921fbcf41c9a0d3e4f6031de14997
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-08-31 23:07:27 +0200

    add new style

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import 'normalize.css';
-import { css } from '@emotion/react';
+// import { css } from 'styled-components';
+// import { css } from 'styled-components';
 
 
 export const AppWrapper = styled.div`
@@ -11,154 +12,155 @@ export const AppWrapper = styled.div`
   
 `;
 
-export const GlobalStyles = css`
+// export const GlobalStyles = css`
 
-:root {
-  --font-family: "Right Grotesk", sans-serif;
-  --second-family: "Messina Sans Mono", sans-serif;
-  --third-family: "Biro Script Plus", sans-serif;
-  --font3: "Formular", sans-serif;
-  --font4: "Roboto", sans-serif;
-  --textColorWhite: #fff;
-}
-@font-face {
-  font-family: '';
-  // src: url() format('truetype');
-}
-  @font-face {
+// :root {
+//   --font-family: "Right Grotesk", sans-serif;
+//   --second-family: "Messina Sans Mono", sans-serif;
+//   --third-family: "Biro Script Plus", sans-serif;
+//   --font3: "Formular", sans-serif;
+//   --font4: "Roboto", sans-serif;
+//   --textColorWhite: #fff;
+// }
+// @font-face {
 //   font-family: '';
-  // src: url() format('truetype');
-}
+//   // src: url() format('truetype');
+// }
+//   @font-face {
+// //   font-family: '';
+//   // src: url() format('truetype');
+// }
 
-@font-face {
-  font-family: '';
-  src: url() format('truetype');
-}
+// @font-face {
+//   font-family: '';
+//   src: url() format('truetype');
+// }
 
-@font-face {
-  font-family: '';
-  src: url() format('truetype');
-}
+// @font-face {
+//   font-family: '';
+//   src: url() format('truetype');
+// }
 
-@font-face {
-  font-family: '';
-  src: url() format('truetype');
-}
+// @font-face {
+//   font-family: '';
+//   src: url() format('truetype');
+// }
 
-@font-face {
-  font-family: '';
-  src: url() format('truetype');
-}
+// @font-face {
+//   font-family: '';
+//   src: url() format('truetype');
+// }
 
-@keyframes move {
-  from {
-    tranform: translateX(0%);
-  }
+// @keyframes move {
+//   from {
+//     tranform: translateX(0%);
+//   }
 
-  to {
-    transform: translateX(-100%);
-  }
-}
+//   to {
+//     transform: translateX(-100%);
+//   }
+// }
   
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    --v1: calc(max(9vw, 11vh));
-    scrollbar-width: none;
-  }
-  body {
+//   * {
+//     margin: 0;
+//     padding: 0;
+//     box-sizing: border-box;
+//     --v1: calc(max(9vw, 11vh));
+//     scrollbar-width: none;
+//   }
+//   body {
 
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+//   margin: 0;
+//   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+//     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+//     sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
 
- font-family: 'Roboto', sans-serif;
-  background-color: #0129;
-  background-size: auto 100%;
-  background-position: center center;
-  background-repeat: cover;
-  font-family: 'Roboto', sans-serif;
-  font-weight: 400;
-  font-style: normal;
-  color: #111111;
-  width: 100%;
-  margin: 0;
-  transition-duration: 300ms;
-  overflow: hidden;
-  }
+//  font-family: 'Roboto', sans-serif;
+//   background-color: #0129;
+//   background-size: auto 100%;
+//   background-position: center center;
+//   background-repeat: cover;
+//   font-family: 'Roboto', sans-serif;
+//   font-weight: 400;
+//   font-style: normal;
+//   color: #111111;
+//   width: 100%;
+//   margin: 0;
+//   transition-duration: 300ms;
+//   overflow: hidden;
 
-  button {
-    cursor: pointer;
-    transition-duration: 300ms;
-  }
-  button:focus,
-  button:hover {
-    // background-color: lightgray; 
-    // box-shadow: 0 0 3px 2px rgba(0, 0, 0, 0.6); 
-  }
+//   }
 
-  a {
-    cursor: pointer;
-    transition-duration: 300ms;
-    text-decoration: none;
-    color: #FFFFFF;
-  }
+//   button {
+//     cursor: pointer;
+//     transition-duration: 300ms;
+//   }
+//   button:focus,
+//   button:hover {
+//     // background-color: lightgray; 
+//     // box-shadow: 0 0 3px 2px rgba(0, 0, 0, 0.6); 
+//   }
+
+//   a {
+//     cursor: pointer;
+//     transition-duration: 300ms;
+//     text-decoration: none;
+//     color: #FFFFFF;
+//   }
  
 
-  img {
-    cursor: pointer;
-    transition-duration: 300ms;
-  }
+//   img {
+//     cursor: pointer;
+//     transition-duration: 300ms;
+//   }
 
 
 
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  p,
- {
-  font-family: 'Messina Sans Mono';
-    margin: 0;
-    padding: 0;
-  }
+//   h1,
+//   h2,
+//   h3,
+//   h4,
+//   h5,
+//   h6,
+//   p,
+//  {
+//   font-family: 'Messina Sans Mono';
+//     margin: 0;
+//     padding: 0;
+//   }
 
-  ul,
-  li {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
+//   ul,
+//   li {
+//     list-style: none;
+//     padding: 0;
+//     margin: 0;
+//   }
 
-  img {
-    display: block;
-    width: 100%;
-    height: auto;
-  }
+//   img {
+//     display: block;
+//     width: 100%;
+//     height: auto;
+//   }
 
-  html {
-    overflow-x: hidden;
-    scroll-behavior: smooth;
+//   html {
+//     overflow-x: hidden;
+//     scroll-behavior: smooth;
    
-    }
+//     }
 
-    @keyframes move {
-  from {
-    tranform: translateY(0%);
-  }
+//     @keyframes move {
+//   from {
+//     tranform: translateY(0%);
+//   }
 
-  to {
-    transform: translateY(-100%);
-  }
-}
+//   to {
+//     transform: translateY(-100%);
+//   }
+// }
  
 
 
 
-`;
+// `;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import { Global, ThemeProvider } from "@emotion/react";
-import { GlobalStyles } from "./App.styled";
+import { ThemeProvider } from "styled-components";
+// import { GlobalStyles } from "./App.styled";
 import App from "./App";
 import { lightTheme } from "./styles/theme";
+// import { Global } from "@emotion/react";
+import GlobalStyles from "./styles/GlobalStyles";
+// import { GlobalStyles } from "./App.styled";
 
 const currentTheme = lightTheme;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={currentTheme}>
-      <Global styles={GlobalStyles} />
+      <GlobalStyles />
       <BrowserRouter basename="/standart-template">
         <App />
       </BrowserRouter>
