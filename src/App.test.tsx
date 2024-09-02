22b1f1e10f8bc7fe4c24bb3dbc07172fc1118d8f
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-09-02 17:37:29 +0200

    test 4

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from 'react';
+import { MemoryRouter } from 'react-router-dom';  // Додаємо MemoryRouter
 
 test('renders learn react link', () => {
-  render(<App />);
+  // eslint-disable-next-line testing-library/no-unnecessary-act
+  act(() => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+  });
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
